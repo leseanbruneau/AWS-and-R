1046a476f4a1482211cbd373d8993ff6f63f9c61
LeSean Bruneau <eaganshark2009@gmail.com>
2018-08-02 12:48:30 +0000

    dos2unix on script files

--- R/Script33_install_R_sudo.sh
+++ R/Script33_install_R_sudo.sh
@@ -3,7 +3,7 @@
 # NOTE:  These scripts need to be run with root access
 #  >  sudo ./Script33_install_R_sudo.sh
 
-cd ~/lib/R-3.5.1
+cd /home/ec2-user/lib/R-3.5.1
 
 #install takes about 
 make install
@@ -20,7 +20,7 @@ export RSTUDIO_WHICH_R=/home/ec2-user/R-3.5.1/bin/R
 
 
 #install RStudio
-cd ~/lib
+cd /home/ec2-user/lib
 
 yum -y install --nogpgcheck rstudio-server-rhel-1.1.456-x86_64.rpm
 
