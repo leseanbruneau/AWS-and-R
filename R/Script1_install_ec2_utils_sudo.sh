1c51445eae0e8e129a903bf221c1fb13ee084c40
EC2 Default User <ec2-user@ip-172-31-92-201.ec2.internal>
2018-09-05 03:01:07 +0000

    fix pcre error

--- R/Script1_install_ec2_utils_sudo.sh
+++ R/Script1_install_ec2_utils_sudo.sh
@@ -9,6 +9,8 @@ echo 'Install utilities needed for R installation...'
 yum -y install make libX11-devel.* libICE-devel.* libSM-devel.* libdmx-devel.* libx* xorg-x11* libFS* libX*  readline-devel gcc-gfortran gcc-c++ texinfo tetex
 yum -y install bzip2-devel.x86_64 bzip2-libs.x86_64 bzip2.x86_64
 yum -y install libcurl libcurl-devel
+yum -y install pcre pcre-devel
 yum -y install java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel.x86_64
 yum -y install openssl-devel
+
 
